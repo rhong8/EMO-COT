9d41dcae422fb38b331ae0fb73165712c3727758
Ryan Hong <ryanhong2014@gmail.com>
2026-07-03 10:35:05 -0400

    turned off existing files option for now

--- meld/build_graph_v3.py
+++ meld/build_graph_v3.py
@@ -157,7 +157,7 @@ def get_relation_with_llm(utterance, audio_features, sentiment):
 #Builds the full emotion graph based on files from all_features.csv and saves it to emotion_graph_dir
 def build_emotion_graph(emotion_graph_dir):
     print("Building the emotion graph...")
-    existing_files = os.listdir(emotion_graph_dir)
+    #existing_files = os.listdir(emotion_graph_dir)
     i = 1
     pipe.model.generation_config.max_length = None #surpress the warning that max_new_tokens take precedence
     for idx, row in features_df.iterrows():
@@ -169,11 +169,11 @@ def build_emotion_graph(emotion_graph_dir):
         output_filename = f"emotion_graph_{filename.replace('.wav', '')}.json"
         
         output_path = os.path.join(emotion_graph_dir, output_filename)
-        
+        '''
         if output_filename in existing_files:
             print(f"{output_filename} has already been processed. skipping")
             continue
-        
+        '''
 
 
         utterance = row['Utterance']
