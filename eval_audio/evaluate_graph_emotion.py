d3b6fe35c7ae917323c6d09a99a99112fabcf024
Ryan Hong <ryanhong2014@gmail.com>
2026-07-02 22:54:35 -0400

    testing prints

--- eval_audio/evaluate_graph_emotion.py
+++ eval_audio/evaluate_graph_emotion.py
@@ -105,9 +105,9 @@ def collate_fn(inputs, processor):
     print(f"Audio waveform shape: {input_audios[0].shape}")
 
        
-    print(f"input_ids shape: {inputs['input_ids'].shape}")
-    print(f"Keys in inputs: {list(inputs.keys())}")
-    
+    #print(f"input_ids shape: {inputs['input_ids'].shape}")
+    #print(f"Keys in inputs: {list(inputs.keys())}")
+
     inputs = processor(text=input_texts, audio=input_audios, sampling_rate=processor.feature_extractor.sampling_rate, return_tensors="pt", padding=True)
     return inputs, audio_path, source, gt
 
